36f893dc6a5dba65e46ae3cf02bc197a0bad91f8
Anders T <andwar1992@gmail.com>
2025-05-07 13:13:06 +0200

    Test 2

---
+++ test 2/test 2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace test_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hei, Verden!");
+        }
+    }
+}
